4f93877fe884ddc31b0ec7b35e6fc254a9762cbc
엄태권 <akfls221@gmail.com>
2024-06-10 03:23:55 +0900

    feat : add configurationproperteis and properties file

---
+++ coin-producer/src/main/kotlin/coin/client/UpBitProperties.kt
@@ -0,0 +1,29 @@
+package com.example.coin.client
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.web.util.UriComponentsBuilder
+import java.net.URI
+
+@ConfigurationProperties(value = "coin.upbit.api")
+class upbitProperties(
+    private val baseUrl: String,
+    private val marketCodeUrl: String,
+    private val candlesMinutesUrl: String
+) {
+    fun marketCodeRequestUri(): URI {
+        return baseUriBuilder()
+            .path(marketCodeUrl)
+            .build().toUri()
+    }
+
+    fun candlesMinutesRequestUri(marketCode: String): URI {
+        return baseUriBuilder()
+            .path(candlesMinutesUrl)
+            .queryParam("market", marketCode)
+            .build().toUri()
+    }
+
+    private fun baseUriBuilder(): UriComponentsBuilder {
+        return UriComponentsBuilder.fromHttpUrl(baseUrl)
+    }
+}
