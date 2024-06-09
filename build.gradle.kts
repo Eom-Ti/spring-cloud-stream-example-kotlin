2c149ab40da6b3d433c8a80bfef5deb1980f6953
엄태권 <akfls221@gmail.com>
2024-06-10 02:47:58 +0900

    feat : setting multi project kotlin build.gradle

---
+++ build.gradle.kts
@@ -0,0 +1,76 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val projectGroup: String by project
+val applicationVersion: String by project
+val javaVersion: String by project
+
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.spring") apply false
+    id("org.springframework.boot") apply false
+    id("io.spring.dependency-management")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(javaVersion)
+    }
+}
+
+allprojects {
+    group = projectGroup
+    version = applicationVersion
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+}
+
+val subProjects = arrayOf(":coin-producer")
+
+subProjects.forEach { subProject ->
+    project(subProject){
+        dependencies {
+            implementation("org.springframework.boot:spring-boot-starter-web")
+            implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+            implementation("org.jetbrains.kotlin:kotlin-reflect")
+            implementation("org.springframework.cloud:spring-cloud-stream")
+            implementation("org.springframework.cloud:spring-cloud-stream-binder-rabbit")
+            implementation("org.springframework.cloud:spring-cloud-stream-binder-kafka")
+//        developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+            testImplementation("org.springframework.boot:spring-boot-starter-test")
+            testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+            testImplementation("org.springframework.cloud:spring-cloud-stream-test-binder")
+            testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+        }
+
+        java {
+            toolchain {
+                languageVersion = JavaLanguageVersion.of(javaVersion)
+            }
+        }
+
+        dependencyManagement {
+            imports {
+                mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+            }
+        }
+
+        kotlin {
+            compilerOptions {
+                freeCompilerArgs.addAll("-Xjsr305=strict")
+            }
+        }
+
+        tasks.withType<Test> {
+            useJUnitPlatform()
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    val springBootVersion: String by settings
+    val springDependencyManagementVersion: String by settings
+    val kotlinVersion: String by settings
+
+    resolutionStrategy{
+        eachPlugin{
+            when(requested.id.id){
+                "org.springframework.boot" -> useVersion(springBootVersion)
+                "io.spring.dependency-management" -> useVersion(springDependencyManagementVersion)
+                "org.jetbrains.kotlin.jvm", "org.jetbrains.kotlin.plugin.spring" -> useVersion(kotlinVersion)
+            }
+        }
+    }
+}
+
+rootProject.name = "spring-cloud-stream-example-kotlin"
+include(
+    ":coin-producer"
+)
+
