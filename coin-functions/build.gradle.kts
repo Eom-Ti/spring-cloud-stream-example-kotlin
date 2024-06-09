80efd231644d6861abe3925a5af08f011c916b8e
엄태권 <akfls221@gmail.com>
2024-06-10 03:23:17 +0900

    feat : add coin functions project

---
+++ coin-functions/build.gradle.kts
@@ -0,0 +1,10 @@
+dependencies {
+    // start objectMapper
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
+
+    // SLF4J API
+    implementation("org.slf4j:slf4j-api:2.0.13")
+
+    // Logback Classic (SLF4J Impl)
+    implementation("ch.qos.logback:logback-classic:1.4.12")
+}
