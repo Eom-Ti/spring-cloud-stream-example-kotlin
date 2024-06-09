696a2e10207dad95b873f470af84bebec55f84ce
엄태권 <akfls221@gmail.com>
2024-06-10 00:57:52 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/springcloudstreamexamplekotlin/SpringCloudStreamExampleKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springcloudstreamexamplekotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCloudStreamExampleKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringCloudStreamExampleKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springcloudstreamexamplekotlin/SpringCloudStreamExampleKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springcloudstreamexamplekotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCloudStreamExampleKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
