243a1cf077685762577540ca063ff1f02e50f10b
엄태권 <akfls221@gmail.com>
2024-06-11 02:47:57 +0900

    feat : add UpBitApiClientImpl class

---
+++ coin-producer/src/main/kotlin/coin/UpBitApiClientImpl.kt
@@ -0,0 +1,47 @@
+package com.example.coin
+
+import com.example.coin.client.UpBitProperties
+import com.example.coin.data.upbit.UpBitMarketCode
+import com.example.logger
+import org.springframework.stereotype.Component
+import org.springframework.web.client.RestClient
+import org.springframework.web.client.body
+
+@Component
+class UpBitApiClientImpl(
+
+    private val upBitProperties: UpBitProperties,
+    private val restClient: RestClient,
+
+) : CoinApiClient {
+
+    lateinit var marketCodes: List<String>
+    val log = logger()
+
+    override fun getCandlesByMinute(marketCode: String): String {
+        return "test"
+    }
+
+    override fun getMarketCodes(): List<String> {
+        if (marketCodes.isNotEmpty()) {
+            return marketCodes
+        }
+
+        val requestUri = upBitProperties.marketCodeRequestUri()
+        log.info("[UpbitApiClientImpl.getMarketCodes]requestUri: $requestUri")
+
+        val stringMarketCodes = restClient.get()
+            .uri(requestUri)
+            .retrieve()
+            .body<List<UpBitMarketCode>>()
+
+        log.info("[UpbitApiClientImpl.getMarketCode] marketCodes: $stringMarketCodes")
+
+        marketCodes = stringMarketCodes
+            ?.filter { it.marketCode.startsWith("KRW") }
+            ?.map { it.marketCode }
+            ?.toList() ?: listOf()
+
+        return marketCodes
+    }
+}
