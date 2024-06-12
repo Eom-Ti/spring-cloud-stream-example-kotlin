13234257f37aa14ddfc500bb27d461f4f8536e43
엄태권 <akfls221@gmail.com>
2024-06-13 04:17:13 +0900

    feat : fix UpBitMarketData file

--- coin-producer/src/main/kotlin/com/example/coin/client/data/upbit/UpBitMarketData.kt
+++ coin-producer/src/main/kotlin/com/example/coin/client/data/upbit/UpBitMarketData.kt
@@ -1,8 +1,13 @@
 package com.example.coin.client.data.upbit
 
 import com.example.coin.client.data.CoinMarketData
+import com.fasterxml.jackson.annotation.JsonProperty
 
 data class UpBitMarketData(
+
+    @field:JsonProperty("market")
     override val marketCode: String,
+
+    @field:JsonProperty("english_name")
     override val marketName: String
 ) : CoinMarketData
