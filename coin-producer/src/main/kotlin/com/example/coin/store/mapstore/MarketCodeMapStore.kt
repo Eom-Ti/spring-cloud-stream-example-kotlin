2e1090a8af9d34915fea32e7cb8c23d90abe2b00
엄태권 <akfls221@gmail.com>
2024-06-12 23:55:43 +0900

    fix : fix store implement

--- coin-producer/src/main/kotlin/com/example/coin/store/mapstore/MarketCodeMapStore.kt
+++ coin-producer/src/main/kotlin/com/example/coin/store/mapstore/MarketCodeMapStore.kt
@@ -1,10 +1,11 @@
-package com.example.coin.store.liststore
+package com.example.coin.store.mapstore
 
 import com.example.coin.client.CoinApiClient
+import com.example.coin.client.data.CoinMarketData
 import com.example.coin.store.MarketCodeStore
+import com.example.com.example.logger
 import org.springframework.beans.factory.InitializingBean
 import org.springframework.stereotype.Component
-import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ConcurrentMap
 
@@ -14,17 +15,22 @@ class MarketCodeMapStore(
     private val coinApiClient: CoinApiClient
 ) : MarketCodeStore, InitializingBean {
 
-    override fun put(marketCode: String) {
-        mapStore.put(marketCode, )
+    val log = logger()
+
+    override fun put(coinMarketData: CoinMarketData) {
+        mapStore[coinMarketData.marketName] = coinMarketData.marketCode
     }
 
-    override fun get(): List<String> {
-        return listStore.toList()
+    override fun findAll(): List<String> {
+        return mapStore.values.toList()
     }
 
     override fun refresh() {
-        listStore.clear()
-        coinApiClient.getMarketCodeList()
+        log.info("Refreshing MarketCodeMapStore")
+        mapStore.clear()
+
+        val coinMarketCode = coinApiClient.getMarketCodeList()
+        coinMarketCode.forEach { mapStore[it.marketName] = it.marketCode }
     }
 
     override fun afterPropertiesSet() {
