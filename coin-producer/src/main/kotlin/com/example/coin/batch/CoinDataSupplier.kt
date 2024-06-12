b7d1653f0fd8255df99abbdd1af7b14b8dc0ac45
엄태권 <akfls221@gmail.com>
2024-06-13 04:18:41 +0900

    feat : fix consumer bean file

--- coin-producer/src/main/kotlin/com/example/coin/batch/CoinDataSupplier.kt
+++ coin-producer/src/main/kotlin/com/example/coin/batch/CoinDataSupplier.kt
@@ -1,26 +1,34 @@
 package com.example.coin.batch
 
-import com.example.coin.client.CoinApiClient
+import com.example.coin.service.CoinDataScrapService
+import kotlinx.coroutines.runBlocking
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.support.KafkaHeaders
+import org.springframework.messaging.Message
+import org.springframework.messaging.support.MessageBuilder
+import java.nio.charset.StandardCharsets
 import java.util.function.Supplier
 
 @Configuration
 class CoinDataSupplier(
-    private val coinApiClient: CoinApiClient
+    private val coinDataScrapService: CoinDataScrapService
 ) {
-
-    @Bean
-    fun createCoinDataSupplier(): Supplier<String> {
-        return Supplier {
-            val marketCodes = coinApiClient.getMarketCodeList()
-            println(marketCodes)
-
-            println("@@@@@@@@@@@")
-
-            "test"
-        }
-
+    companion object {
+        const val MESSAGE_KEY = "coinRawData"
     }
 
+    @Bean
+    fun createCoinDataSupplier(): Supplier<Message<String>> {
+        return Supplier {
+            val stringCoinCandle = runBlocking {
+                coinDataScrapService.scrapMarketCoinMinuteCandle()
+            }
+
+            MessageBuilder
+                .withPayload(stringCoinCandle)
+                .setHeader(KafkaHeaders.KEY, MESSAGE_KEY.toByteArray(StandardCharsets.UTF_8))
+                .build()
+        }
+    }
 }
