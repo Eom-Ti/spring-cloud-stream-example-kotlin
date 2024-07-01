ce50d78af61cddfe6b8a4be36050f8c04684990b
엄태권 <akfls221@gmail.com>
2024-07-02 02:18:42 +0900

    fix : fix private -> val

--- coin-functions/src/main/kotlin/com/example/coin/data/message/Candle.kt
+++ coin-functions/src/main/kotlin/com/example/coin/data/message/Candle.kt
@@ -3,4 +3,4 @@ package com.example.coin.data.message
 import com.example.com.example.com.example.coin.data.message.CandleDetail
 import java.io.Serializable
 
-class Candle(private val candleDetails: List<CandleDetail>): Serializable
+class Candle(val candleDetails: List<CandleDetail>): Serializable
